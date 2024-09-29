a2dac2154f8cf27b94283ee75bdaceb538d181f1
WAndriessen <willem868@gmail.com>
2024-09-29 10:48:24 +0200

    Add function to get number or words and refactor

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    
-print(file_contents)
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(num_words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+main()
